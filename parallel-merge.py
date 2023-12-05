6e086b540b3f3eadc71ff4ef94ef6b050843b332
Dedad Fajar <139190249+VictoriaEstara@users.noreply.github.com>
2023-12-05 19:22:28 +0800

    Add files via upload

---
+++ parallel-merge.py
@@ -0,0 +1,41 @@
+import concurrent.futures
+
+def parallel_merge(arr1, arr2):
+    result = [0] * (len(arr1) + len(arr2))
+    i = j = k = 0
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] < arr2[j]:
+                futures.append(executor.submit(copy_value, arr1[i], result, k))
+                i += 1
+            else:
+                futures.append(executor.submit(copy_value, arr2[j], result, k))
+                j += 1
+            k += 1
+
+        while i < len(arr1):
+            futures.append(executor.submit(copy_value, arr1[i], result, k))
+            i += 1
+            k += 1
+
+        while j < len(arr2):
+            futures.append(executor.submit(copy_value, arr2[j], result, k))
+            j += 1
+            k += 1
+
+    # Wait for all futures to complete
+    concurrent.futures.wait(futures)
+
+    return result
+
+def copy_value(value, result, index):
+    result[index] = value
+
+# Contoh penggunaan
+arr1 = [1, 3, 5, 7]
+arr2 = [2, 4, 6, 8]
+
+result = parallel_merge(arr1, arr2)
+print("\nHasil Merge:", result)
